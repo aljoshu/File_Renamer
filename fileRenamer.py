e8fe66cc238e8e6afafbaaaedd079035b381942e
Alvin Joshua <alvinjoshua93@gmail.com>
2023-12-14 12:09:23 +0700

    first commit
    need to fix image resize and add the rename feature

---
+++ fileRenamer.py
@@ -0,0 +1,99 @@
+import os
+import cv2 as cv
+from tkinter import *
+import tkinter as tk
+import PIL.ImageTk
+import PIL.Image
+
+# Variables
+pictsDirectory = 'pictures'
+stdWidth = 500
+stdHeight = 500
+stdSize = (stdWidth, stdHeight)
+current = 0
+allPicts = []
+
+# Iterate through each file in the 'pictures' directory
+print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+print(" Below are the files in the pictures folder VVV")
+print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+for filename in os.listdir(pictsDirectory):
+  if("DS_Store" in filename):
+    print("Invalid File Found!")
+  else:
+    fullName = os.path.join(pictsDirectory, filename) # pictures/example.jpg
+    print(fullName)
+    allPicts.append(fullName)
+
+
+root = tk.Tk()                        # m is the name of the main window
+root.title('File Renamer')
+frame = Frame(root)                   # use frame as the widget container
+frame.pack()
+bottomframe = Frame(root)             # make a frame at the bottom for allignment
+bottomframe.pack(side=BOTTOM)
+
+nameVar = tk.StringVar()
+def submit():
+  oldName = allPicts[current]
+  newName = nameVar.get()
+  if(len(newName) != 0):
+    print("The new name for [" + oldName + "] is : " + newName)
+    nameVar.set("")
+
+title = tk.Label(frame, text=allPicts[current], fg='red')    # Prints the current picture name
+title.pack()
+
+currImg = PIL.Image.open(allPicts[current])           # Open the current picture
+currImg = currImg.resize(stdSize)                     # Resize the image to our liking
+photoImg = PIL.ImageTk.PhotoImage(currImg)
+imageLabel = tk.Label(image=photoImg)                 # Put the image in a Label widget
+imageLabel.image = photoImg
+imageLabel.pack()
+
+indexStr = str(current + 1) + "/" + str(len(allPicts))
+def next():
+  global current
+  # update the picture index value
+  if(current < len(allPicts) - 1):
+    current += 1
+    updateIndex = str(current + 1) + "/" + str(len(allPicts))
+    pictIndex.config(text=updateIndex)
+    title.config(text=allPicts[current])
+    #print("current index: " + updateIndex)
+
+  # update the current display picture
+  nextImg = PIL.Image.open(allPicts[current])
+  nextImg = nextImg.resize(stdSize)
+  nextPI = PIL.ImageTk.PhotoImage(nextImg)
+  imageLabel.configure(image=nextPI)
+  imageLabel.image = nextPI
+
+def prev():
+  global current
+  # update the picture index value
+  if(current > 0):
+    current -= 1
+    updateIndex = str(current + 1) + "/" + str(len(allPicts))
+    pictIndex.config(text=updateIndex)
+    #print("current index: " + updateIndex)
+
+  # update the current display
+  prevImg = PIL.Image.open(allPicts[current])
+  prevImg = prevImg.resize(stdSize)
+  prevPI = PIL.ImageTk.PhotoImage(prevImg)
+  imageLabel.configure(image=prevPI)
+  imageLabel.image = prevPI
+
+Label(frame, text='new name ').pack(side=LEFT)        # label for the new name textbox/ entry
+nameEntry = Entry(frame, textvariable=nameVar)        # make the textbox/ entry
+nameEntry.pack(side=LEFT)
+prevBtn = tk.Button(bottomframe, text='< prev ', fg='blue', command=prev)
+prevBtn.pack(side=LEFT)
+submitBtn = tk.Button(frame, text='Submit', fg='green', command=submit)  # make the save button
+submitBtn.pack(side=LEFT)
+pictIndex = Label(bottomframe, text=indexStr)         # put the picture index in a label widget
+pictIndex.pack(side=LEFT)
+nextBtn = tk.Button(bottomframe, text=' next >', fg='blue', command=next)
+nextBtn.pack(side=LEFT)
+root.mainloop()               # an infinite loop that runs as long as the main window isn't closed
